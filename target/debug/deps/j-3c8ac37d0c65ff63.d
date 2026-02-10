999f300e064bb631babd0b032c179da11478000e
lingojack <3065225677@qq.com>
2026-02-10 23:00:11 +0800

    init

---
+++ target/debug/deps/j-3c8ac37d0c65ff63.d
@@ -0,0 +1,5 @@
+/Users/jacklingo/Documents/Code/work_copilot/j/target/debug/deps/j-3c8ac37d0c65ff63.d: src/main.rs
+
+/Users/jacklingo/Documents/Code/work_copilot/j/target/debug/deps/libj-3c8ac37d0c65ff63.rmeta: src/main.rs
+
+src/main.rs:
