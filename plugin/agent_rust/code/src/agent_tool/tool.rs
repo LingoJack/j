c099c0b7b0ff19e8a1c20f0038bcf5a0b907fefc
gougouwen <gougouwen@tencent.com>
2026-03-08 00:21:54 +0800

    更新: 2026-03-08 00:21:54

--- plugin/agent_rust/code/src/agent_tool/tool.rs
+++
@@ -1,8 +0,0 @@
-pub mod agent_tool;
-
-pub trait AgentTool {
-    fn name(&self) -> &'static str;
-    fn description(&self) -> &'static str;
-    fn param_schema(&self) -> serde_json::Value;
-    fn execute(&self);
-}
